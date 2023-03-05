6863f4a9e13a4a1905a0e4fc4012019545cfb1f9
wyyaa123 <863114354@qq.com>
2023-03-05 21:06:23 +0800

    添加注释

--- include/SNL.hpp
+++ include/SNL.hpp
@@ -31,7 +31,6 @@ namespace SNL {
      * @param THRESHOLD 时间阈值
      * @param subs 订阅者队列
      * @param pubs 发布者队列
-     * @param time ros的Time类
      * @param datas 订阅到的话题数据
      */
     class Comunicate  {
@@ -60,25 +59,46 @@ namespace SNL {
             template<typename _TyPtr1, typename _TyPtr2>
             std::deque<std::any>::iterator AilgnMsgs(const _TyPtr2& msgconPtr);
     };
-
+    /**
+     * @brief 向某一话题消息类型的容器队列尾添加元素
+     * 
+     * @tparam _TyPtr 消息类型
+     * @param msgconPtr 要添加的消息数据
+     */
     template <typename _TyPtr>
     void Comunicate::emplace(_TyPtr& msgconPtr) {
         std::string TP = typeid(msgconPtr).name();
         datas[TP].emplace_back(msgconPtr);
     }
-
+    /**
+     * @brief 返回对应消息类型队列的元素个数
+     * 
+     * @tparam _TyPtr 消息类型
+     * @return size_t 
+     */
     template <typename _TyPtr>
     size_t Comunicate::size() {
         std::string TP = typeid(_TyPtr).name();
         return datas[TP].size();
     }
-
+    /**
+     * @brief 删除对应消息类型队列的第一个元素
+     * 
+     * @tparam _TyPtr 消息类型
+     */
     template <typename _TyPtr>
     void Comunicate::pop_front() {
         std::string TP = typeid(_TyPtr).name();
         return datas[TP].pop_front();
     }
-
+    /**
+     * @brief 得到与输入元素时间最对应的_TyPtr1类型的话题数据
+     * 
+     * @tparam _TyPtr1 要与输入消息匹配的话题数据类型
+     * @tparam _TyPtr2 输入的消息类型
+     * @param msgconPtr 输入的消息
+     * @return _TyPtr1 
+     */
     template<typename _TyPtr1, typename _TyPtr2>
     _TyPtr1 Comunicate::getAilgnedMsg(const _TyPtr2& msgconPtr) {
 
@@ -91,12 +111,13 @@ namespace SNL {
         } 
         return _TyPtr1();
     }
-
     /**
      * @brief 返回第一个满足在阈值范围内的话题数据的迭代器，并将其之前的话题数据全部删除
      * 
-     * @param coorAtCamPtr 用于配准的话题数据
-     * @return std::deque<geometry_msgs::PointStampedConstPtr>::iterator 
+     * @tparam _TyPtr1 要与输入消息匹配的话题数据类型
+     * @tparam _TyPtr2 输入的消息类型
+     * @param msgconPtr 用于配准的话题数据
+     * @return std::deque<std::any>::iterator 
      */
     template<typename _TyPtr1, typename _TyPtr2>
     std::deque<std::any>::iterator Comunicate::AilgnMsgs(const _TyPtr2& msgconPtr) {
@@ -157,6 +178,78 @@ namespace SNL {
      */
     size_t Comunicate::subSize() && {
         return subs.size();
+    }
+
+
+    /**
+     * @brief 相机类
+     * 
+     */
+    class Cam {
+    public:
+        Cam() { this->Intrinsic << fx, 0, cx, 0, fy, cy, 0, 0, 1; };
+        /**
+         * @brief Construct a new Cam object
+         * 
+         * @param fx 内参系数
+         * @param fy 内参系数
+         * @param cx 内参系数
+         * @param cy 内参系数
+         */
+        Cam(double fx, double fy, double cx, double cy) : fx(fx), fy(fy), cx(cx), cy(cy) { this->Intrinsic << fx, 0, cx, 0, fy, cy, 0, 0, 1; }
+        geometry_msgs::PointStamped pixel2Cam(const Eigen::Vector2d& pixelCoor, float depth) &;
+        geometry_msgs::PointStamped cam2UAV(const geometry_msgs::PointStampedConstPtr& camCoor, bool flag);
+    private:
+        Eigen::Matrix3d Intrinsic;
+        const double fx = 376.0;
+        const double fy = 376.0;
+        const double cx = 376.0;
+        const double cy = 240.0;
+    };
+
+    /**
+     * @brief 返回相机坐标系下的[x, y, z], 返回值只能做右值
+     * 
+     * @param pixelCoor 像素坐标系下的[x, y]
+     * @param depth 像素点的深度，默认1
+     * @return nav_msgs::Odometry 
+     */
+    geometry_msgs::PointStamped Cam::pixel2Cam(const Eigen::Vector2d& pixelCoor, float depth = 1) & {
+        std::cout << "在rgb像素坐标系下的坐标为: (" << pixelCoor[0] << ", " << pixelCoor[1] << ") " << std::endl;
+        std::cout << "目标点深度为:" << depth  << "m" << std::endl;
+        // std::cout << "相机内参为:" << this->fx << ", " << this->fy << ", " << this->cx << ", " << this->cy << std::endl;
+        Eigen::Vector3d coor = { pixelCoor[0], pixelCoor[1], 1 }; 
+        Eigen::Vector3d xyz = this->Intrinsic.inverse() * depth * coor;
+        geometry_msgs::PointStamped Point;
+        Point.point.x = xyz[0];
+        Point.point.y = xyz[1];
+        Point.point.z = xyz[2];
+        std::cout << "在相机坐标系下的坐标为: (" << xyz[0] << ", " 
+                                             << xyz[1] << ", "
+                                             << xyz[2] << ") "
+                                             << std::endl;
+        std::cout << "------------------------------" << std::endl;
+        return Point;
+    } 
+    /**
+     * @brief 返回机体坐标系下的[x, y, z], 返回值只能做右值
+     * 
+     * @param camCoor 相机坐标系下的坐标
+     * @param flag 坐标变换规则
+     * @return nav_msgs::Odometry 
+     */
+    geometry_msgs::PointStamped Cam::cam2UAV(const geometry_msgs::PointStampedConstPtr& camCoor, bool flag = 1) {
+        geometry_msgs::PointStamped AircaftCoor;
+        if(flag) {
+            AircaftCoor.point.x = camCoor->point.z;
+            AircaftCoor.point.y = -camCoor->point.x;
+            AircaftCoor.point.z = -camCoor->point.y;
+        } else {
+            AircaftCoor.point.x = camCoor->point.y;
+            AircaftCoor.point.y = -camCoor->point.x;
+            AircaftCoor.point.z = -camCoor->point.z;  
+        }
+        return AircaftCoor;
     }
 };
 
