4efeae26f24451558997d83067e7bc1ee7f97de2
wyyaa123 <863114354@qq.com>
2023-03-14 21:51:32 +0800

    2023年3月14日

--- test_OpenCV/src/main.cpp
+++ test_OpenCV/src/main.cpp
@@ -22,7 +22,11 @@ void findCornerPoints (int argc, char** argv) {
     cv::cvtColor(img, gray_img, cv::COLOR_BGR2GRAY); //使用灰度图进行角点检测。
     vector<cv::Point2f> corner_points;
 
-    cv::goodFeaturesToTrack(gray_img, corner_points, 100, 0.1, 10); //查找角点
+    cv::goodFeaturesToTrack(gray_img, corner_points, 100, 0.1, 10); //查找像素级角点
+
+    cv::TermCriteria criteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 40, 0.01);
+
+    cv::cornerSubPix(gray_img, corner_points, cv::Size(5, 5), cv::Size(-1, -1), criteria);
 
     for(auto& point : corner_points) {
         auto random1 = getRandomNum(0, 255);
